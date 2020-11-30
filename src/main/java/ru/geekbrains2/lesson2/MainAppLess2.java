b5ce3a93e4d101d1f7315d2245e0198b93fd77cd
GudkovDmitry <gudkovdm@gmail.com>
2020-11-30 11:13:27 +0300

    lesson2 in progress

--- src/main/java/ru/geekbrains2/lesson2/MainAppLess2.java
+++ src/main/java/ru/geekbrains2/lesson2/MainAppLess2.java
@@ -1,2 +1,54 @@
-package ru.geekbrains2.lesson2;public class MainAppLess2 {
+package ru.geekbrains2.lesson2;
+
+import java.util.Arrays;
+
+import static java.lang.Math.random;
+
+public class MainAppLess2 {
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+        String []a = {
+                "1","2","3","4","5","6","7","8","9",
+                "0","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"};
+        String [][]m = new String [4][4];
+
+        for (int i = 0; i < m.length ; i++) {
+            for (int j = 0; j < m[i].length ; j++) {
+                m[i][j] = a[(int)(Math.random()*a.length)];
+            }
+        }
+
+        getArray(m);
+
+    }
+
+    public static void getArray(String[][] m) throws MyArraySizeException,MyArrayDataException{
+        if (m.length != 4) throw new MyArraySizeException("Массив должен быть 4x4");
+        for (int i = 0; i < m.length ; i++) {
+            if (m[i].length != 4) throw new MyArraySizeException("Массив должен быть 4x4");
+        }
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                
+            }
+        }
+        //System.out.println("ура!!!! передан шикарный массив");
+
+    }
+
+
 }
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
+
+
+
