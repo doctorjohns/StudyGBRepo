75e72ecab9f9423f19d2a7e3d7c5c32f6cea25f6
GudkovDmitry <gudkovdm@gmail.com>
2020-12-01 11:44:17 +0300

    Homework lesson2 finished

--- src/main/java/ru/geekbrains2/lesson2/MainAppLess2.java
+++ src/main/java/ru/geekbrains2/lesson2/MainAppLess2.java
@@ -1,59 +1,52 @@
 package ru.geekbrains2.lesson2;
 
-import java.util.Arrays;
-
-import static java.lang.Math.random;
+import com.sun.javafx.binding.StringFormatter;
 
 public class MainAppLess2 {
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
-        //массив для исключений
-        String []a = {
-                "1","2","3","4","5","6","7","8","9",
-                "0","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"};
-        //массив с интами
-        String []a2 = {"1","2","3","4","5","6","7","8","9"};
-        String [][]m = new String [5][4];
+        String[] a = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "q"};
+        String[][] m = new String[4][4];
 
-        for (int i = 0; i < m.length ; i++) {
-            for (int j = 0; j < m[i].length ; j++) {
-                m[i][j] = a2[(int)(Math.random()*a2.length)];
-            }
-        }
-
-        getArray(m);
-
-    }
-
-    public static void getArray(String[][] m) throws MyArraySizeException,MyArrayDataException{
-        int ss=0;
-        if (m.length != 4) throw new MyArraySizeException("Массив должен быть 4x4");
-        for (int i = 0; i < m.length ; i++) {
-            if (m[i].length != 4) throw new MyArraySizeException("Массив должен быть 4x4");
-        }
+        //заполняю массив рандомными значениями из массива а
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) {
-                ss += Integer.parseInt(m[i][j]);
+                m[i][j] = a[(int) (Math.random() * a.length)];
             }
         }
-        System.out.println(ss);
-        //System.out.println("ура!!!! передан шикарный массив");
+        //смотрим размерность массива
+        getArray(m);
+        //считаем сумму всех членов массива
+        int sumA = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                try {
+                    sumA += Integer.parseInt(m[i][j]);
+                } catch (RuntimeException e) {
+                    throw new MyArrayDataException(String.format("\nВ массиве по координатам %d %d лежит не int! Ты не пройдешь!",i,j));
+                }
+            }
+        }
+        System.out.printf("Сумма всех членов массива = %d",sumA);
 
     }
-
-
+    //проверяем размер массива
+    public static void getArray(String[][] m) throws MyArraySizeException {
+        if (m.length != 4) throw new MyArraySizeException("Разрешен массив только размером 4x4");
+        for (int i = 0; i < m.length; i++) {
+            if (m[i].length != 4) throw new MyArraySizeException("Разрешен массив только размером 4x4");
+        }
+    }
 }
-
-class MyArraySizeException extends Exception {
+//мои эксепшены
+class MyArraySizeException extends Exception{
     public MyArraySizeException(String message) {
         super(message);
     }
 }
-class MyArrayDataException extends Exception {
+class MyArrayDataException extends Exception{
     public MyArrayDataException(String message) {
         super(message);
     }
+
 }
-
-
-
 
