0099457fbb29d08fe1bd8a16ce9f0a913586493b
GudkovDmitry <gudkovdm@gmail.com>
2020-12-08 08:20:51 +0300

    Homework Lesson4 finished

--- src/main/java/ru/geekbrains2/lesson4/ChatApp.java
+++ src/main/java/ru/geekbrains2/lesson4/ChatApp.java
@@ -1,2 +1,41 @@
-package ru.geekbrains2.lesson4;public class ChatApp {
+package ru.geekbrains2.lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChatApp extends JFrame {
+    public ChatApp() {
+        setTitle ("ChatRoom");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(200,100,600,800);
+
+
+        JTextArea chatText = new JTextArea();
+        chatText.setEnabled(false);
+
+        JScrollPane scrollPane = new JScrollPane(chatText);
+        add(scrollPane, BorderLayout.CENTER);
+
+        JTextField userText = new JTextField();
+        userText.setColumns(2);
+
+        JButton sendText = new JButton();
+        sendText.setText("Отправить");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,1));
+        panel.add(userText);
+        panel.add(sendText);
+        add(panel,BorderLayout.SOUTH);
+
+        sendText.addActionListener(e -> {
+            chatText.append(userText.getText()+"\n");
+            userText.setText("");
+        });
+        userText.addActionListener(e -> {
+            chatText.append(userText.getText()+"\n");
+            userText.setText("");
+        });
+        setVisible(true);
+    }
 }
--- src/main/java/ru/geekbrains2/lesson4/MainAppLess4.java
+++ src/main/java/ru/geekbrains2/lesson4/MainAppLess4.java
@@ -1,2 +1,7 @@
-package ru.geekbrains2.lesson4;public class MainAppLess4 {
+package ru.geekbrains2.lesson4;
+
+public class MainAppLess4 {
+    public static void main(String[] args) {
+        new ChatApp();
+    }
 }
